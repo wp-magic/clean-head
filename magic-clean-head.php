0ba25877c437d8503dceff3a687a1013c04fc844
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-11 14:28:03 +0200

    remove wp api header field, formatting

--- magic-clean-head.php
+++ magic-clean-head.php
@@ -24,13 +24,13 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 add_action( 'init', function() {
-  remove_action('wp_head', 'wp_generator');
-  remove_action ('wp_head', 'rsd_link');
-  remove_action( 'wp_head', 'wlwmanifest_link');
-  remove_action( 'wp_head', 'wp_shortlink_wp_head');
-  remove_action('wp_head', 'wp_resource_hints', 2);
+  remove_action( 'wp_head', 'wp_generator' );
+  remove_action( 'wp_head', 'rsd_link' );
+  remove_action( 'wp_head', 'wlwmanifest_link' );
+  remove_action( 'wp_head', 'wp_shortlink_wp_head' );
+  remove_action( 'wp_head', 'wp_resource_hints', 2 );
   remove_action( 'wp_head', 'feed_links', 2 );
-  remove_action('wp_head', 'feed_links_extra', 3 );
-
-  remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+  remove_action( 'wp_head', 'feed_links_extra', 3 );
+  remove_action( 'wp_head', 'rest_output_link_wp_head' );
+  remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head' );
 } );
